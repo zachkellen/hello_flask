ceba33ce368aee1f773015a17a3306994a731b09
Zach Kellen <z.kellen12@gmail.com>
2021-08-06 16:14:22 -0500

    Rendering views and template engines

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template("index.html")  # Return the string 'Hello World!' as a response
 
 @app.route('/success')
 def success():
@@ -10,7 +10,7 @@ def success():
 
 @app.route(('/hello/<string:name>/<int:num>'))
 def hello(name,num):
-    return f"Hello {name * num}"
+    return render_template("hello.html", name = name, num = num)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
